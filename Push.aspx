857d387cdcaa7759f5b7db94a0de371ff7a9d1a5
samercs <samer_mail_2006@yahoo.com>
2016-06-01 11:15:35 +0300

    Add Push Notification

---
+++ Push.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Push.aspx.cs" Inherits="Push" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>
+                    Text
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="btnSend" runat="server" Text="Send Notification" OnClick="btnSend_Click" />
+                </td>
+                
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Push.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Azure.NotificationHubs;
+
+public partial class Push : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSend_Click(object sender, EventArgs e)
+    {
+        string notificationHubName = "test";
+        string notificationHubConnection =
+            "Endpoint=sb://orangejetpack.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=uMJHQThBSc1EsIcMms3PyTasVUW1cwhaAdyZSAHraXU=";
+
+        NotificationHubClient hub = NotificationHubClient
+    .CreateClientFromConnectionString(notificationHubConnection, notificationHubName);
+
+        var androidNotificationPayload = "{ \"data\" : {\"message\":\"" + TextBox1.Text + "\"}}";
+
+        try
+        {
+            //var result = await hub.SendGcmNativeNotificationAsync(androidNotificationPayload);
+            hub.SendGcmNativeNotificationAsync(androidNotificationPayload)
+                .ContinueWith(FailSend, TaskContinuationOptions.OnlyOnFaulted);
+            
+            Response.Write("Notification has been send");
+        }
+        catch (System.Exception ex)
+        {
+            // Write the failure result to the logs.
+            Response.Write("Push.SendAsync Error, "+ ex.Message);
+        }
+    }
+
+    public void FailSend(Task task)
+    {
+        Response.Write(task.Exception);
+    }
+}
